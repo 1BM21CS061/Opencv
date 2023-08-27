529a2bb12b30b941dee4efa5244b29cb1f5d7a23
G SAI VIKRANT <saivikrant.cs21@bmsce.ac.in>
2023-08-28 04:32:12 +0530

    Editing image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -1,23 +1,40 @@
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 def display(img,name):
     while True:
         cv2.imshow(name,img)
 
-        if cv2.waitKey(0) & 0xFF == ord('q'):
+        if cv2.waitKey(0) == ord('q'):
             cv2.destroyAllWindows()
             break
 
-img = cv2.imread("./KOA_Nassau_2697x1517.jpg")
+img = cv2.imread("./41g7KY1DWgL._AC_UF1000,1000_QL80_.jpg")
+
 display(img,"original")
-img[:,:,0:2] = 0
+
+img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+display(img_gray,"grayed")
+
+mask_img = cv2.bitwise_not(img_gray)
+display(mask_img,"mask")
+
+'''white_background = np.full(img.shape,255,dtype=np.uint8)
+bk = cv2.bitwise_or(white_background,white_background,mask = mask_img)
+display(bk,"mask_c")
+'''
+
+display(cv2.bitwise_and(img,img,mask=mask_img),"result")
+
+#print(bk.shape)
+'''img[:,:,0:2] = 0
 display(img,"isolated")
-img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 display(img_gray,"grayed")
 
 mask_inv = cv2.bitwise_not(img_gray)
 display(mask_inv,"mask")
 
 bk = cv2.bitwise_or(img,img,mask= mask_inv)
-display(bk,"answer")
+display(bk,"answer")'''
